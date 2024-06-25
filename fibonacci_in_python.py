0c8847e01f039c8ef3bddb04d7f7c859514a5de6
Babatunde Mudathir Koiki <38375959+Babatunde13@users.noreply.github.com>
2024-06-25 16:12:58 +0100

    Update and rename fibonacci in python to fibonacci_in_python.py

--- fibonacci_in_python.py
+++ fibonacci_in_python.py
@@ -1,16 +1,11 @@
-'''This is a basic python script'''
+''' This is a basic Python script'''
 
 def fib(n):
-
     if n == 0 or n == 1:
-
         return 1
-
     else:
-
         return fib(n - 1) + fib(n - 2)
 
 if __name__=='__main__':
     n = 9
     print(fib(n - 1) + fib(n - 2))
-
